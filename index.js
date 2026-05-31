6e99d7e4c99c09f20f9d198ff21a2ce6d40ad62b
Gary <ronny0american@gmail.com>
2026-05-31 20:31:09 +1000

    Верстка главного экрана

---
+++ index.js
@@ -0,0 +1,29 @@
+function board() {
+    const board = document.querySelector(".board");
+    const folder = document.querySelector(".folder");
+    const analytics = document.querySelector(".analytics");
+
+    board.style.display = "flex";
+    folder.style.display = "none";
+    analytics.style.display = "none";
+}
+
+function folder() {
+    const board = document.querySelector(".board");
+    const folder = document.querySelector(".folder");
+    const analytics = document.querySelector(".analytics");
+
+    board.style.display = "none";
+    folder.style.display = "flex";
+    analytics.style.display = "none";
+}
+
+function analitics() {
+    const board = document.querySelector(".board");
+    const folder = document.querySelector(".folder");
+    const analytics = document.querySelector(".analytics");
+
+    board.style.display = "none";
+    folder.style.display = "none";
+    analytics.style.display = "flex";
+}
